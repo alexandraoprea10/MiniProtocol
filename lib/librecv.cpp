b969717029f9e0ca9930ad5472230c816b4795b3
Alexandra Oprea <adrianaalex07@gmail.com>
2026-05-27 16:44:42 +0300

    Implementare aproape finala. Nu merge Selective Repeat.

--- lib/librecv.cpp
+++ lib/librecv.cpp
@@ -18,22 +18,31 @@ struct pollfd data_fds[MAX_CONNECTIONS];
 /* Used for timers per connection */
 struct pollfd timer_fds[MAX_CONNECTIONS];
 int fdmax = 0;
+// creez un socket prin care receiver-ul asteapta pachete
 int listenfd;
 
 int recv_data(int conn_id, char *buffer, int len)
 {
     int size = 0;
+    // extragem conexiunea cu ID-ul conn_id
     struct connection *current_connection = cons[conn_id];
+    // blochez mutex-ul pentru ulterioarele modificari ce vin asupra pachetului
     pthread_mutex_lock(&cons[conn_id]->con_lock);
+    // astept ca buffer-ul sa primeasca un pachet
     while (current_connection->packet.empty()) {
         pthread_cond_wait(&current_connection->wait_data, &current_connection->con_lock);
     }
     /* We will write code here as to not have sync problems with recv_handler */
+    // initializam nr de bytes cu lungimea initiala oferita ca argument
     int copy_buffer = len;
+    // daca lungimea este mai mica, o actualizam
     if ((int)current_connection->packet.size() < len)
         copy_buffer = current_connection->packet.size();
+    // copiez in buffer data-ul din pachet(pachetul contine header + data)
     memcpy(buffer, current_connection->packet.data(), copy_buffer);
+    // stergem datele pe care tocmai le-am copiat
     current_connection->packet.erase(current_connection->packet.begin(), current_connection->packet.begin() + copy_buffer);
+    // deblocam mutex-ul dupa ce am terminat de modificat conexiunea curenta
     pthread_mutex_unlock(&cons[conn_id]->con_lock);
 
     return copy_buffer;
@@ -50,61 +59,82 @@ void *receiver_handler(void *arg)
         int conn_id = -1;
         do {
             res = recv_message_or_timeout(segment, MAX_SEGMENT_SIZE, &conn_id);
+            // daca gasim vreo eroare, nu facem nimic
+            // daca res == -1, atunci am primit o eroare si iesim din bucla
             if (res == -1)
                 break;
+            // daca res <= 0, atunci am primit un pachet gol si trebuie sa rulam pana primim ceva
             if (res <= 0)
                 continue;
 
         } while(res == -14);
+        // caut pachetul in ceea ce am primit deja. daca apartine, atunci trecem mai departe
         auto idx = cons.find(conn_id);
         if (idx == cons.end()) {
             continue;
         }
+        // extragem structura din pachetul gasit
         struct connection *current_connection = idx->second;
+        // blocam mutex-ul pentru ulterioarele modificari ale conexiunii curente
         pthread_mutex_lock(&cons[conn_id]->con_lock);
 
         /* Handle segment received from the sender. We use this between locks
         as to not have synchronization issues with the recv_data calls which are
         on the main thread */
 
+        // daca n-am primit pachet complet, nu il modific si trec mai departe
         if (res < (int)sizeof(struct poli_tcp_ctrl_hdr)) {
             pthread_mutex_unlock(&current_connection->con_lock);
             continue;
         }
-
+        
+        // daca am primit pachet complet, atunci il extrag din buffer-ul curent
         struct poli_tcp_data_hdr *header = (struct poli_tcp_data_hdr *)segment;
+        // daca pachetul nu respecta ID-ul corespunzator sau nu are tipul 0, nu il modific si trec mai departe
         if (header->protocol_id != POLI_PROTOCOL_ID || header->type != 0) {
             pthread_mutex_unlock(&current_connection->con_lock);
             continue;
         }
 
+        // extrag numarul de secventa si lungimea datelor pusa in header
         int seq = ntohs(header->seq_num);
         int length = ntohs(header->len);
+        // veridic numarul de bytes din pachet
         int current_empty_space = res - sizeof(struct poli_tcp_data_hdr);
+        // daca am ceva mai mare decat cat avm liber, trunchiez la ce putem primi
         if (length > current_empty_space) {
             length = current_empty_space;
         }
         if (length > 0) {
+            // calculez de unde incep datele din pachet
             char *start_buffer = segment + sizeof(struct poli_tcp_data_hdr);
+            // creez un vector in care pun doar datele care mi trebuie
             std::vector<char> buffer(start_buffer, start_buffer + length);
+            // daca pachetul are numarul de secventa egal cu ceea ce astept 
             if (seq == current_connection->expected_seq) {
+                // adaug noul pachet si incrementez numarul de secventa asteptat
                 current_connection->packet.insert(current_connection->packet.end(), buffer.begin(), buffer.end());
                 current_connection->expected_seq++;
                 pthread_cond_signal(&current_connection->wait_data);
+                // caut daca exista deja pachetul, pentru a nu-l pune de doua ori
                 auto idx = current_connection->receive_packet.find(current_connection->expected_seq);
                 while (idx != current_connection->receive_packet.end()) {
+                    // adaug pachetul la sfarsitul vectorului 
                     current_connection->packet.insert(current_connection->packet.end(), idx->second.begin(), idx->second.end());
                     current_connection->expected_seq++;
+                    // sterg pachetul mutat mai sus pentru a fi codul mai rapid
                     current_connection->receive_packet.erase(idx);
                     idx = current_connection->receive_packet.find(current_connection->expected_seq);
                     pthread_cond_signal(&current_connection->wait_data);
                 }
             } else {
+                // daca pachetul are nr de secventa mai mare decat ceea ce astept, atunci il adaug in lista de asteptare pana primesc ceea ce lipseste deja
                 if (seq > current_connection->expected_seq || (current_connection->expected_seq > 65000 && seq < 100)) {
                     current_connection->receive_packet[seq] = buffer;
                 }
             }
         }
+        // creez pachetul de raspuns catre sender
         struct poli_tcp_ctrl_hdr resp;
         resp.ack_num = htons((uint16_t) seq);
         resp.protocol_id = POLI_PROTOCOL_ID;
@@ -121,6 +151,8 @@ int wait4connect(uint32_t ip, uint16_t port)
     /* TODO: Implement the Three Way Handshake on the receiver part. This blocks
      * until a connection is established. */
 
+    // creez o noua structura (daca lasam doar alocarea, nu treceau testele)
+    // am cautat documentatia din C++ si trebuie apelat si un constructie
     struct connection *con = (struct connection *)malloc(sizeof(struct connection));
     new (con) struct connection();
     // struct connection *con = new struct connection();
@@ -135,6 +167,8 @@ int wait4connect(uint32_t ip, uint16_t port)
     if (setsockopt(con->sockfd, SOL_SOCKET, SO_RCVTIMEO,&tv,sizeof(tv)) < 0) {
         perror("Error");
     } */
+
+    // creez o structura a clientului si astept raspunsul de pe socket
     struct sockaddr_in client_addr;
     socklen_t clen = sizeof(client_addr);
     char raspuns[MAX_SEGMENT_SIZE];
@@ -147,24 +181,34 @@ int wait4connect(uint32_t ip, uint16_t port)
     /* Receive SYN on the connection socket. Create a new socket and bind it to
      * the chosen port. Send the data port number via SYN-ACK to the client */
     con->sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    // am modificat buffer-ul si l-am setat la 1024, chiar daca MAX_SEGMENT_SIZE este 512
+    // l-am marit ca sa ma asigur ca este destul spatiu si impart corect pachetele
     int buffer = 1024 * 1024;
     setsockopt(con->sockfd, SOL_SOCKET, SO_RCVBUF, &buffer, sizeof(buffer));
     
+    // creez o structura de raspuns
     struct sockaddr_in resp;
+    // initializez campurile, deoarece caut pachetul I4V4, de tip 0, cu orice adresa
     resp.sin_addr.s_addr = htonl(INADDR_ANY);
     resp.sin_family = AF_INET;
     resp.sin_port = 0;
+    // caut apchetul 
     bind(con->sockfd, (struct sockaddr *)&resp, sizeof(resp));
 
+    // verific ce port a fost ales de socket
     socklen_t clen2 = sizeof(resp);
     getsockname(con->sockfd, (struct sockaddr *)&resp, &clen2);
 
+    // creez o structura pentru raspuns
     struct poli_tcp_ctrl_hdr header;
     header.ack_num = resp.sin_port;
     header.protocol_id = POLI_PROTOCOL_ID;
+    // type = 3 deoarece pachetul este de tip SYN_ACK
     header.type = 3;
+    // trimit pachetul inapoi la client
     sendto(listenfd, &header, sizeof(header), 0, (struct sockaddr *)&client_addr, clen);
 
+    // salvez clientul ca sa stiu cui trimit
     memcpy(&con->servaddr, &client_addr, sizeof(client_addr));
     con->expected_seq = 0;
     pthread_cond_init(&con->wait_data, NULL);
@@ -178,7 +222,8 @@ int wait4connect(uint32_t ip, uint16_t port)
        to know if a timeout has happend on a connection */
     timer_fds[fdmax].fd = timerfd_create(CLOCK_REALTIME,  0);    
     timer_fds[fdmax].events = POLLIN;    
-    struct itimerspec spec;     
+    struct itimerspec spec;
+    // nu am modificat timpul aici, pentru ca nu imi mai treceau testele     
     spec.it_value.tv_sec = 1;    
     spec.it_value.tv_nsec = 0;    
     spec.it_interval.tv_sec = 1;    
@@ -200,16 +245,21 @@ void init_receiver(int recv_buffer_bytes)
     int ret;
 
     /* TODO: Create the connection socket and bind it to 8031 */
+    // creez conexiunea
     listenfd = socket(AF_INET, SOCK_DGRAM, 0);
 
+    // am modificat buffer-ul si l-am setat la 1024, chiar daca MAX_SEGMENT_SIZE este 512
+    // l-am marit ca sa ma asigur ca este destul spatiu si impart corect pachetele
     int buffer = 1024 * 1024;
     setsockopt(listenfd, SOL_SOCKET, SO_RCVBUF, &buffer, sizeof(buffer));
 
-    struct sockaddr_in client_addr;
-    client_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    client_addr.sin_family = AF_INET;
-    client_addr.sin_port = htons(8032);
-    bind(listenfd, (struct sockaddr *)&client_addr, sizeof(client_addr));
+    // creez o structura a server-ului
+    struct sockaddr_in server_addr;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8032);
+    // caut pachetele de pe portul 8032, cu pachete de orice tip si orice IP
+    bind(listenfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
 
     ret = pthread_create( &thread1, NULL, receiver_handler, NULL);
     assert(ret == 0);
--- lib/libsend.cpp
+++ lib/libsend.cpp
@@ -21,46 +21,60 @@ struct pollfd data_fds[MAX_CONNECTIONS];
 struct pollfd timer_fds[MAX_CONNECTIONS];
 int fdmax = 0;
 
+// dimensiunea ferestrei
 int nr_ferestre = 600;
 
 int send_data(int conn_id, char *buffer, int len)
 {
+    // extragem conexiunea cu ID-ul conn_id
     struct connection *current_connection = cons[conn_id];
     int ferestre_curente = 0;
     while (1) {
         pthread_mutex_lock(&current_connection->con_lock);
+        // daca nu s-a umplut fereastra, ma opresc si ies
         if (current_connection->next_to_send - current_connection->base < nr_ferestre) {
             break;
         }
+        // deblochez mutex-ul pentru a primi ACK
         pthread_mutex_unlock(&current_connection->con_lock);
+        // cresc nr de ferestre
         ferestre_curente++;
+        // daca primesc prea multe ferestre, blochez mutex-ul si ies
         if (ferestre_curente > 1000) {
             pthread_mutex_lock(&current_connection->con_lock);
             break;
         }
-        usleep(100);
+        // astept 150ms pentru a verifica din nou(trimit multe pachete deodata si sa nu se suprapuna)
+        usleep(150);
     }
 
     /* We will write code here as to not have sync problems with sender_handler */
+    // creez pachetul pe care vreau sa il trimit
     struct poli_tcp_data_hdr header;
     header.conn_id = current_connection->conn_id;
     header.len = htons(len);
     header.protocol_id = POLI_PROTOCOL_ID;
-    header.seq_num = htons(current_connection->next_to_send % 65536);
+    header.seq_num = htons(current_connection->next_to_send);
     header.type = 0;
 
+    // creez un vector care sa contine header-ul si datele
     std::vector<char> send_packet(sizeof(header) + len);
+    // inital, copiez header-ul
     memcpy(send_packet.data(), &header, sizeof(header));
+    // apoi, copiez datele dupa header
     memcpy(send_packet.data() + sizeof(header), buffer, len);
 
+    // salvez pachetul creat, in cazul in care se primeste un ACK gresit si este nevoie sa l retransmit
     current_connection->sent_packet[current_connection->next_to_send] = send_packet;
     current_connection->next_to_send++;
 
+    // trimit pachetul(ca in laboratorul 6/7)
     int rc = sendto(current_connection->sockfd, send_packet.data(), send_packet.size(), 0, (struct sockaddr *)&current_connection->servaddr, sizeof(current_connection->servaddr));
+    // deblochez mutex-ul dupa ce am terminat de modificat conexiunea curenta
     pthread_mutex_unlock(&current_connection->con_lock);
     if (rc < 0)
         return -1;
-
+    // returnez lungimea datelor pe care am trimis-o
     return len;
 }
 
@@ -77,15 +91,20 @@ void *sender_handler(void *arg)
         do {
             res = recv_message_or_timeout(buf, MAX_SEGMENT_SIZE, &conn_id);
             if (res == -1 || res == -14) {
+                // caut conexiunea cu ID-ul 0
                 auto again = cons.find(0);
                 if (again != cons.end()) {
                     struct connection *current_connection = again->second;
                     pthread_mutex_lock(&current_connection->con_lock);
-
+                    // verific daca exista pachete care nu sunt gata de trimis
                     if (!current_connection->sent_packet.empty()) {
                         auto idx2 = current_connection->sent_packet.begin();
+                        // am implementat Go Back n - stiu ca era recomandat Selective Repeat, dar nu am reusit sa il fac sa ruleze corect
+                        // parcurg toate pachetele pe care nu le-am trimis
                         while (idx2 != current_connection->sent_packet.end()) {
+                            // extragem pachetul curent
                             const std::vector <char>& packet = idx2->second;
+                            // trimit pachetul din nou
                             sendto(current_connection->sockfd, packet.data(), packet.size(), 0, (struct sockaddr *)&current_connection->servaddr, sizeof(current_connection->servaddr));
                             idx2++;
                         }
@@ -102,31 +121,42 @@ void *sender_handler(void *arg)
         if (idx == cons.end()) {
             continue;
         }
+        // extrag conexiunea valida
         struct connection *current_connection = idx->second;
+        // blocare mutex ca sa nu modific starea
         pthread_mutex_lock(&current_connection->con_lock);
 
         /* Handle segment received from the receiver. We use this between locks
         as to not have synchronization issues with the send_data calls which are
         on the main thread */
         if (res >= (int)sizeof(struct poli_tcp_ctrl_hdr)) {
+            // extrag pachetul din buffer
             struct poli_tcp_ctrl_hdr *header = (struct poli_tcp_ctrl_hdr*) buf;
+            // verific daca ppachetul are ID-ul corespunzator si daca tipul pachetului este 1
             if (header->protocol_id == POLI_PROTOCOL_ID && header->type == 1) {
+                // extrag ACK
                 int ack = ntohs(header->ack_num);
+                // sterg pachetul cu ACK-ul respectiv din vectorul de pachete
                 current_connection->sent_packet.erase(ack);
                 int old = current_connection->base;
+                // merg mai departe cu urmatorul pachet- glisare
                 while (current_connection->base < current_connection->next_to_send) {
                     int idx2 = current_connection->base;
+                    // daca pachetul inca nu a primit ACK, iesim din bucla, trebuie retransmis
                     if (current_connection->sent_packet.count(idx2) > 0) {
                         break;
                     }
                     current_connection->base++;
                 }
+                // verific daca s-a mutat fereastra
                 if (current_connection->base == old && ack > current_connection->base) {
                     const std::vector<char>& packet = current_connection->sent_packet[current_connection->base];
+                    // daca da, atunci pachetul nu a primit ACK corect si trebuie retransmis
                     sendto(current_connection->sockfd, packet.data(), packet.size(), 0, (struct sockaddr *)&current_connection->servaddr, sizeof(current_connection->servaddr));
                 }
             }
         }
+        // deblochez mutex-ul dupa modificari
         pthread_mutex_unlock(&current_connection->con_lock);
     }
     return NULL;
@@ -137,27 +167,34 @@ int setup_connection(uint32_t ip, uint16_t port)
     /* Implement the sender part of the Three Way Handshake. Blocks
     until the connection is established */
 
+    // creez o noua structura (daca lasam doar alocarea, nu treceau testele)
+    // am cautat documentatia din C++ si trebuie apelat si un constructie
     struct connection *con = (struct connection *)malloc(sizeof(struct connection));
     new (con) struct connection();
     // struct connection *con = new struct connection();
     int conn_id = 0;
     con->sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
+    // am modificat buffer-ele si le-am setat la 1024, chiar daca MAX_SEGMENT_SIZE este 512
+    // le-am marit ca sa ma asigur ca este destul spatiu si impart corect pachetele
     int buffer = 1024 * 1024;
     setsockopt(con->sockfd, SOL_SOCKET, SO_SNDBUF, &buffer, sizeof(buffer));
     setsockopt(con->sockfd, SOL_SOCKET, SO_RCVBUF, &buffer, sizeof(buffer));
 
+    // initializez campurile structurii create mai sus
     con->base = 0;
     con->next_to_send = 0;
     con->servaddr.sin_addr.s_addr = ip;
     con->servaddr.sin_family = AF_INET;
     con->servaddr.sin_port = port;
 
+    // creez un alt pachet pentru conexiune
     struct poli_tcp_ctrl_hdr header;
     header.protocol_id = POLI_PROTOCOL_ID;
     header.type = 2;
     sendto(con->sockfd, &header, sizeof(header), 0, (struct sockaddr *)&con->servaddr, sizeof(con->servaddr));
 
+    // aloc o memorie pentur un nou buffer pentru pachetul pe care il voi primi
     char buff[MAX_SEGMENT_SIZE];
     struct sockaddr_in client_addr;
     socklen_t clen = sizeof(client_addr);
@@ -165,6 +202,7 @@ int setup_connection(uint32_t ip, uint16_t port)
 
     if (rc >= (int)sizeof(struct poli_tcp_ctrl_hdr)) {
         struct poli_tcp_ctrl_hdr *resp = (struct poli_tcp_ctrl_hdr *)buff;
+        // salvam ce am primit, ca sa stim unde trimitem restul pachetelor
         con->servaddr.sin_port = resp->ack_num;
     }
     /* // This can be used to set a timer on a socket 
@@ -190,6 +228,7 @@ int setup_connection(uint32_t ip, uint16_t port)
     timer_fds[fdmax].events = POLLIN;    
     struct itimerspec spec;     
     spec.it_value.tv_sec = 0;
+    // am modificat timpul la 20000000 pentru ca sunt multe pachete si sa nu se suprapuna
     spec.it_value.tv_nsec = 20000000;    
     spec.it_interval.tv_sec = 0;
     spec.it_interval.tv_nsec = 20000000;    
